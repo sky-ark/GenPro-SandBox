d9cb885bd0a1bc6c3345ab8caeb681ffacb51d2c
sky-ark <a.renaux@nouvellesimages.xyz>
2024-10-16 22:19:50 +0200

    Delaunay working
    stocks delaunay triangles in a list delaunayTriangles.

--- Assets/Scripts/Delaunay.cs
+++ Assets/Scripts/Delaunay.cs
@@ -8,45 +8,50 @@ public class Delaunay : MonoBehaviour
     public BSP bsp;
     private List<Rect> rects;
     private List<Vector2> points;
-    private List<Triangle> triangles;
-    
+    private List<Triangle> allTriangles;
+    private List<Triangle> delaunayTriangles;
+
     [ContextMenu("Launch")]
     private void Launch()
     {
-        //Call bsp to launch the rectangles calculation
+        // Call bsp to launch the rectangles calculation
         bsp.Launch();
-        //Get the rectangles from the BSP script
+        // Get the rectangles from the BSP script
         rects = bsp.GetRects();
-        //Generate points from the centers of the rectangles
+        // Generate points from the centers of the rectangles
         GeneratePointsFromRects();
-        //Perform Delaunay triangulation
+        // Perform Delaunay triangulation
         Triangulation();
-        //Draw triangles (for visualization purposes)
-        DrawTriangles();
+        // Draw all triangles and circles for 3 seconds
+        // DrawTriangles(allTriangles, Color.yellow, 3f);
+        // DrawCircumcircles(allTriangles, Color.red, 3f);
+        // Draw only Delaunay triangles for 10 seconds
+        DrawTriangles(delaunayTriangles, Color.green, 10f);
+        // DrawCircumcircles(delaunayTriangles, Color.magenta, 10f);
     }
-    //Get the rectangles from the BSP script
+
+    // Get the rectangles from the BSP script
     private void GeneratePointsFromRects()
     {
         points = new List<Vector2>();
-        
+
         foreach (var rect in rects)
         {
             Vector2 center = new Vector2(rect.x + rect.width / 2, rect.y + rect.height / 2);
             points.Add(center);
         }
-
-
     }
 
     private void Triangulation()
     {
-        triangles = new List<Triangle>();
+        allTriangles = new List<Triangle>();
+        delaunayTriangles = new List<Triangle>();
 
         for (int i = 0; i < points.Count; i++)
         {
             for (int j = 0; j < points.Count; j++)
             {
-                if ( i!= j ) 
+                if (i != j)
                 {
                     for (int k = 0; k < points.Count; k++)
                     {
@@ -56,28 +61,83 @@ public class Delaunay : MonoBehaviour
                             Vector2 p2 = points[j];
                             Vector2 p3 = points[k];
                             Triangle triangle = new Triangle(p1, p2, p3);
-                            triangles.Add(triangle);
+                            allTriangles.Add(triangle);
+                            if (IsDelaunay(triangle))
+                            {
+                                delaunayTriangles.Add(triangle);
+                            }
                         }
                     }
                 }
             }
         }
     }
-    
-    private void DrawTriangles()
+
+    private bool IsDelaunay(Triangle triangle)
+    {
+        Vector2 center;
+        float radius;
+        GetCircumcircle(triangle, out center, out radius);
+
+        foreach (var point in points)
+        {
+            if (point != triangle.p1 && point != triangle.p2 && point != triangle.p3)
+            {
+                if (Vector2.Distance(center, point) < radius)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void GetCircumcircle(Triangle triangle, out Vector2 center, out float radius)
+    {
+        Vector2 p1 = triangle.p1;
+        Vector2 p2 = triangle.p2;
+        Vector2 p3 = triangle.p3;
+
+        float ax = p1.x;
+        float ay = p1.y;
+        float bx = p2.x;
+        float by = p2.y;
+        float cx = p3.x;
+        float cy = p3.y;
+
+        float d = 2 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by));
+        float ux = ((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) + (cx * cx + cy * cy) * (ay - by)) / d;
+        float uy = ((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) + (cx * cx + cy * cy) * (bx - ax)) / d;
+
+        center = new Vector2(ux, uy);
+        radius = Vector2.Distance(center, p1);
+    }
+
+    private void DrawTriangles(List<Triangle> triangles, Color color, float duration)
     {
         if (triangles == null) return;
         foreach (var triangle in triangles)
         {
-            Debug.DrawLine(triangle.p1, triangle.p2, Color.yellow);
-            Debug.DrawLine(triangle.p2, triangle.p3, Color.yellow);
-            Debug.DrawLine(triangle.p3, triangle.p1,Color.yellow);
+            Debug.DrawLine(triangle.p1, triangle.p2, color, duration);
+            Debug.DrawLine(triangle.p2, triangle.p3, color, duration);
+            Debug.DrawLine(triangle.p3, triangle.p1, color, duration);
+        }
+    }
+
+    private void DrawCircumcircles(List<Triangle> triangles, Color color, float duration)
+    {
+        foreach (var triangle in triangles)
+        {
+            Vector2 center;
+            float radius;
+            GetCircumcircle(triangle, out center, out radius);
+            
         }
     }
 
     class Triangle
     {
-        //Triangle is made up of 3 points
+        // Triangle is made up of 3 points
         public Vector2 p1, p2, p3;
         public Triangle(Vector2 p1, Vector2 p2, Vector2 p3)
         {
@@ -86,5 +146,4 @@ public class Delaunay : MonoBehaviour
             this.p3 = p3;
         }
     }
-    
 }
